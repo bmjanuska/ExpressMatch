f4f1951b50c2601d664730bbb8ff194d676eb412
Bernadette Januska <bmjanuska@gmail.com>
2018-08-24 00:10:16 -0400

    added artists you can match with

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ExpressMatch
 Matching app made with express
+![GitHub Logo](screen.png)
+
+Concept:
+Take the survey to find out what artist you should take on a date!
+
+The app uses cutting edge technology and algorithms for comparing similarities to find your dreamy creative partner.
+
+
+Still in progress
--- app/data/friends.js
+++ app/data/friends.js
@@ -1,33 +1,101 @@
+//Preloaded possible matches
+
 var friendList = [
   {
-    name:"Frank",
-    photo:"#",
+    name:" Frida Kahlo",
+    photo:"http://dazedimg.dazedgroup.netdna-cdn.com/525/azure/dazed-prod/1210/9/1219445.jpg",
     scores:[
-      5,
+      3,
       1,
+      3,
+      3,
       4,
       4,
-      5,
+      3,
+      5
+    ]
+  },
+  {
+    name:"Andy Warhol",
+    photo:"http://2.bp.blogspot.com/-Z_6R_uEu0V0/TyrAuPEfT6I/AAAAAAAAgis/cQ0l-QLTj7A/w1200-h630-p-k-no-nu/Andy_Warhol_by_Jack_Mitchell.jpg",
+    scores:[
       1,
+      5,
+      3,
+      4,
+      4,
       2,
+      1,
+      1
+    ]
+  },
+  {
+    name:"Vincent van Gogh",
+    photo:"https://www.sartle.com/media/artist/vincent-van-gogh.jpg",
+    scores:[
+      4,
+      1,
       5,
+      4,
+      2,
+      1,
+      3,
+      5
+    ]
+  },
+  {
+    name:"Edward Hopper",
+    photo:"http://blogs.getty.edu/iris/files/2010/03/edward_hopper.jpg",
+    scores:[
+      2,
+      4,
+      4,
+      5,
+      2,
+      1,
       4,
       1
     ]
   },
   {
-    name:"Josh",
-    photo:"#",
+    name:"Jackson Pollock",
+    photo:"https://www.jackson-pollock.org/images/jackson_pollock.jpg",
     scores:[
-      2,
-      2,
-      2,
+      1,
+      1,
+      1,
+      1,
+      5,
+      1,
+      1,
+      1
+    ]
+  },
+  {
+    name:"Yoko Ono",
+    photo:"http://dazedimg.dazedgroup.netdna-cdn.com/1048/azure/dazed-prod/1230/9/1239465.jpg",
+    scores:[
       4,
       5,
-      6,
-      1,
+      3,
+      3,
+      4,
+      5,
+      2,
+      3
+    ]
+  },
+  {
+    name:"Salvador Dali",
+    photo:"https://www.theartstory.org/images20/new_design/bio/bio_dali_salvador.jpg",
+    scores:[
+      3,
       2,
       3,
+      1,
+      2,
+      1,
+      2,
       5
     ]
   }
